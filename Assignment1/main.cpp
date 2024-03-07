9733620a8649a44ff3697f37c6ff46f19552f3ca
ROOOO <ql.wang.gd@gmail.com>
2024-03-08 00:09:56 +0800

    [HW1] finished homework 1

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -23,9 +23,14 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
 {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
 
-    // TODO: Implement this function
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
+    float fCos = std::cos(rotation_angle / 180.0f * (float)MY_PI);
+    float fSin = std::sin(rotation_angle / 180.0f * (float)MY_PI);
+    model << fCos, -fSin, 0, 0,
+             fSin,  fCos, 0, 0,
+             0,        0, 1, 0,
+             0,        0, 0, 1;
 
     return model;
 }
@@ -37,11 +42,47 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
 
     Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
 
-    // TODO: Implement this function
     // Create the projection matrix for the given parameters.
     // Then return it.
+    Eigen::Matrix4f ortho2persp;
+    ortho2persp << zNear, 0,     0,            0,
+                   0,     zNear, 0,            0,
+                   0,     0,     zNear + zFar, -zNear * zFar,
+                   0,     0,     1,            0;
+    float halve = eye_fov * 0.5f / 180.0f * (float)MY_PI;
+    float h = std::tan(halve) * zNear;
+    float w = h * aspect_ratio;
+    float l = -w, r = w;
+    float b = -h, t = h;
+    Eigen::Matrix4f ortho;
+    ortho << 2 / (r - l), 0,           0,                  0,
+             0,           2 / (t - b), 0,                  0,
+             0,           0,           2 / (zFar - zNear), 0,
+             0,           0,           0,                  1;
+
+    projection = ortho * ortho2persp;
 
     return projection;
+}
+
+Eigen::Matrix4f get_rotation(Eigen::Vector3f axis, float angle)
+{
+    float alpha = angle / 180.0f * (float)MY_PI;
+    float fCos = std::cos(alpha);
+    float fSin = std::sin(alpha);
+
+    Eigen::Matrix3f I;
+    I.setIdentity();
+
+    Eigen::Matrix3f N;
+    N << 0,         -axis.z(), axis.y(),
+         axis.z(),  0,         -axis.x(),
+         -axis.y(), axis.x(),  0;
+
+    Eigen::Matrix3f rod = fCos * I + (1.0f - fCos) * axis * axis.transpose() + fSin * N;
+    Eigen::Matrix4f result = Eigen::Matrix4f::Identity();
+    result.block<3, 3>(0, 0) = rod;
+    return result;
 }
 
 int main(int argc, const char** argv)
